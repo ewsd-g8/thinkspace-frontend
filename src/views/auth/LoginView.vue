3d0b5ae49c671747acab682461e3121e50b7184e
HtetAung2003 <147127822+HtetAung2003@users.noreply.github.com>
2025-02-10 21:02:23 +0630

    Update LoginView.vue
    color change

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -18,7 +18,7 @@
         />
       </div>
     </div>
-    <div class="auth-fluid-form-box" style="background-color: #fff">
+    <div class="auth-fluid-form-box" style="background-color: #000">
       <div class="align-items-center d-flex h-100">
         <div class="card-body">
           <h4 class="mt-0 fw-bold">Welcome to EDUplus Admin System!</h4>
