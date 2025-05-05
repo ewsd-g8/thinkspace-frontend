112f6912e85e6130943e9d740362b874990df33f
Min Thit Htoo <133160196+Jamy62@users.noreply.github.com>
2025-05-05 16:47:48 +0630

    develop fix (#35)

--- src/components/layouts/TopMenuView.vue
+++ src/components/layouts/TopMenuView.vue
@@ -57,7 +57,7 @@
 
       <!-- LOGO -->
       <div class="logo-box">
-        <a href="index.html" class="logo logo-dark text-center">
+        <a href="idea_post/idea" class="logo logo-dark text-center">
           <span class="logo-sm">
             <img
               src="/icon/think-space.png"
@@ -76,7 +76,7 @@
           </span>
         </a>
 
-        <a href="index.html" class="logo logo-light text-center">
+        <a href="idea_post/idea" class="logo logo-light text-center">
           <span class="logo-sm">
             <img
               src="/icon/think-space-white-logo.png"
